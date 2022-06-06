50eaefbafb9c8c30b325afe1b8b72c5d76fc51d8
Baqir Naqvi <b.naqvi99@gmail.com>
2022-06-06 05:46:09 +0500

    Update admin.js

--- middleware/admin.js
+++ middleware/admin.js
@@ -5,9 +5,12 @@ function admin_middleware(req, res, next) {
     const decode=jwt.verify(req.header('Authorization'),JWTKEY);
     User.findById(decode._id).then(
         (result)=>{
+            if(result.role)
+            {
             if(result.role=="admin"){
             next()
             }
+        }
             else{
              return res.json({message:"You are not admin"});
             }
